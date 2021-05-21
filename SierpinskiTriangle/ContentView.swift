045bba9d219669f297f8b0c228a8c66023abcfe4
Hung-Chun, Tsai <tsaihong1995@gmail.com>
2021-05-20 22:21:30 -0400

    Initial Commit

---
+++ SierpinskiTriangle/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SierpinskiTriangle
+//
+//  Created by Hung-Chun Tsai on 2021-05-20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SierpinskiTriangle/SierpinskiTriangleApp.swift
@@ -0,0 +1,17 @@
+//
+//  SierpinskiTriangleApp.swift
+//  SierpinskiTriangle
+//
+//  Created by Hung-Chun Tsai on 2021-05-20.
+//
+
+import SwiftUI
+
+@main
+struct SierpinskiTriangleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
